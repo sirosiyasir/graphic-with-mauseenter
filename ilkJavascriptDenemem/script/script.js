571f72d23a18b63c5b7b4e2233d84ae8708137fd
Yasir <112541705+sirosiyasir@users.noreply.github.com>
2022-09-30 15:14:31 +0300

    Update script.js

--- ilkJavascriptDenemem/script/script.js
+++ ilkJavascriptDenemem/script/script.js
@@ -8,3 +8,14 @@ div.addEventListener("mouseenter" , () => {
 div.addEventListener("mouseleave" , () => {
     text.style.display = "none"
 })
+
+const div2 = document.querySelector(".down-thu")
+const text2 = document.querySelector(".text-on-hover-2")
+
+div2.addEventListener("mouseenter" , () => {
+  text2.style.display = "block"
+})
+
+div2.addEventListener("mouseleave" , () => {
+  text2.style.display = "none"
+})
